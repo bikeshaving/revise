0005794d39d1bbf57af5337aafb80b5121e71cfe
Brian Kim <briankimpossible@gmail.com>
2019-01-12 09:12:43 -0500

    delete vestigial stuff from sup

--- bench/index.js
+++ bench/index.js
@@ -1,16 +1,11 @@
+// TODO
 const fs = require("fs");
 const path = require("path");
 const Benchmark = require("benchmark");
 const { Suite } = Benchmark;
-
-const sup = require("../lib");
-
 const suite = new Suite();
 
-const readme = fs.readFileSync(path.join(__dirname, "../README.sup"), "utf8");
-suite.add("parse#readme", () => {
-  sup.parse(readme);
-});
+suite.add("parse#readme", () => {});
 
 suite.on("cycle", (ev) => {
   /* eslint-disable-next-line no-console */
