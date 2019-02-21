1720f75e149d23566d37a2478a3e69eaf9c81ce2
Brian Kim <briankimpossible@gmail.com>
2019-02-21 15:52:04 -0500

    use SubseqIterator in ZipIterator

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ export function push(subseq: Subseq, length: number, flag: boolean): number {
   return subseq.length;
 }
 
-type SubseqIteratorValue = [number, boolean];
+export type SubseqIteratorValue = [number, boolean];
 
 export class SubseqIterator implements IterableIterator<SubseqIteratorValue> {
   private i = 1;
@@ -89,44 +89,40 @@ export function complement(subseq: Subseq): Subseq {
   return [subseq[0] ? 0 : 1].concat(subseq.slice(1));
 }
 
-type ZipIteratorValue = [number, boolean, boolean];
+export type ZipIteratorValue = [number, boolean, boolean];
 
 export class ZipIterator implements IterableIterator<ZipIteratorValue> {
-  private consumed = 0;
-  private consumed1 = 0;
-  private consumed2 = 0;
-  private i1 = 1;
-  private i2 = 1;
-  constructor(private subseq1: Subseq, private subseq2: Subseq) {}
+  private iter1: SubseqIterator;
+  private iter2: SubseqIterator;
+  private it1: IteratorResult<SubseqIteratorValue>;
+  private it2: IteratorResult<SubseqIteratorValue>;
+  constructor(subseq1: Subseq, subseq2: Subseq) {
+    this.iter1 = new SubseqIterator(subseq1);
+    this.iter2 = new SubseqIterator(subseq2);
+    this.it1 = this.iter1.next();
+    this.it2 = this.iter2.next();
+  }
 
   next(): IteratorResult<ZipIteratorValue> {
-    const length1 = this.subseq1[this.i1];
-    const length2 = this.subseq2[this.i2];
-    const flag1 = flagAt(this.subseq1, this.i1);
-    const flag2 = flagAt(this.subseq2, this.i2);
-    if (length1 == null || length2 == null) {
-      if (length1 || length2) {
+    if (this.it1.done || this.it2.done) {
+      if (!this.it1.done || !this.it2.done) {
         throw new Error("Length mismatch");
       }
       return { done: true } as IteratorResult<ZipIteratorValue>;
     }
-    let length: number;
-    if (length1 + this.consumed1 === length2 + this.consumed2) {
-      this.consumed1 += length1;
-      this.consumed2 += length2;
-      this.i1++;
-      this.i2++;
-      length = this.consumed1 - this.consumed;
-    } else if (length1 + this.consumed1 < length2 + this.consumed2) {
-      this.consumed1 += length1;
-      this.i1++;
-      length = this.consumed1 - this.consumed;
+    const [length1, flag1] = this.it1.value;
+    const [length2, flag2] = this.it2.value;
+    const length = Math.min(length1, length2);
+    if (length1 - length > 0) {
+      this.it1.value[0] -= length;
     } else {
-      this.consumed2 += length2;
-      this.i2++;
-      length = this.consumed2 - this.consumed;
+      this.it1 = this.iter1.next();
     }
-    this.consumed += length;
+    if (length2 - length > 0) {
+      this.it2.value[0] -= length;
+    } else {
+      this.it2 = this.iter2.next();
+    }
     return {
       done: false,
       value: [length, flag1, flag2],
@@ -146,16 +142,16 @@ export class ZipIterator implements IterableIterator<ZipIteratorValue> {
   }
 }
 
+export function zip(subseq1: Subseq, subseq2: Subseq): ZipIterator {
+  return new ZipIterator(subseq1, subseq2);
+}
+
 export function union(subseq1: Subseq, subseq2: Subseq): Subseq {
-  return new ZipIterator(subseq1, subseq2).join(
-    (flag1, flag2) => flag1 || flag2,
-  );
+  return zip(subseq1, subseq2).join((flag1, flag2) => flag1 || flag2);
 }
 
 export function difference(subseq1: Subseq, subseq2: Subseq): Subseq {
-  return new ZipIterator(subseq1, subseq2).join(
-    (flag1, flag2) => flag1 && !flag2,
-  );
+  return zip(subseq1, subseq2).join((flag1, flag2) => flag1 && !flag2);
 }
 
 export function expand(
@@ -195,7 +191,7 @@ export function expand(
 
 export function shrink(subseq1: Subseq, subseq2: Subseq): Subseq {
   const result: Subseq = [];
-  for (const [length, flag1, flag2] of new ZipIterator(subseq1, subseq2)) {
+  for (const [length, flag1, flag2] of zip(subseq1, subseq2)) {
     if (!flag2) {
       push(result, length, flag1);
     }
@@ -349,7 +345,7 @@ export function synthesize(
   const patch: Patch = [];
   let i = 0; // insert index
   let consumed = 0;
-  for (const [length, iFlag, dFlag] of new ZipIterator(insertSeq, deleteSeq)) {
+  for (const [length, iFlag, dFlag] of zip(insertSeq, deleteSeq)) {
     if (iFlag) {
       i += length;
       if (!dFlag) {
