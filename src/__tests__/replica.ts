a5b603b7a3a097099291d2c26e9a8edf9f2bf425
Brian Kim <briankimpossible@gmail.com>
2019-03-16 22:24:41 -0400

    rename doc to replica in replica tests

--- src/__tests__/replica.ts
+++ src/__tests__/replica.ts
@@ -3,75 +3,75 @@ import { Replica } from "../Replica";
 describe("Replica", () => {
   describe("Replica.hiddenSeqAt", () => {
     test("comprehensive", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit(["H", 1, 6, "W", 7, 11]);
-      doc.edit([0, 5, ", Brian", 11]);
-      doc.edit([0, 5, ", Dr. Evil", 11], { priority: 1, parent: 1 });
-      expect(doc.hiddenSeqAt(0)).toEqual([]);
-      expect(doc.hiddenSeqAt(1)).toEqual([0, 11]);
-      expect(doc.hiddenSeqAt(2)).toEqual([0, 1, 1, 6, 1, 4]);
-      expect(doc.hiddenSeqAt(3)).toEqual([0, 1, 1, 11, 7]);
-      expect(doc.hiddenSeqAt(4)).toEqual([0, 1, 1, 21, 7]);
-      expect(doc.hiddenSeqAt(4)).toEqual(doc.snapshot.hiddenSeq);
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit(["H", 1, 6, "W", 7, 11]);
+      replica.edit([0, 5, ", Brian", 11]);
+      replica.edit([0, 5, ", Dr. Evil", 11], { priority: 1, parent: 1 });
+      expect(replica.hiddenSeqAt(0)).toEqual([]);
+      expect(replica.hiddenSeqAt(1)).toEqual([0, 11]);
+      expect(replica.hiddenSeqAt(2)).toEqual([0, 1, 1, 6, 1, 4]);
+      expect(replica.hiddenSeqAt(3)).toEqual([0, 1, 1, 11, 7]);
+      expect(replica.hiddenSeqAt(4)).toEqual([0, 1, 1, 21, 7]);
+      expect(replica.hiddenSeqAt(4)).toEqual(replica.snapshot.hiddenSeq);
     });
   });
 
   describe("Replica.snapshotAt", () => {
     test("comprehensive", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit(["H", 1, 6, "W", 7, 11]);
-      doc.edit([0, 5, ", Brian", 11]);
-      doc.edit([0, 5, ", Dr. Evil", 11], { priority: 1, parent: 1 });
-      expect(doc.snapshotAt(1)).toEqual({
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit(["H", 1, 6, "W", 7, 11]);
+      replica.edit([0, 5, ", Brian", 11]);
+      replica.edit([0, 5, ", Dr. Evil", 11], { priority: 1, parent: 1 });
+      expect(replica.snapshotAt(1)).toEqual({
         visible: "hello world",
         hidden: "",
         hiddenSeq: [0, 11],
         version: 1,
       });
-      expect(doc.snapshotAt(2)).toEqual({
+      expect(replica.snapshotAt(2)).toEqual({
         visible: "Hello World",
         hidden: "hw",
         hiddenSeq: [0, 1, 1, 6, 1, 4],
         version: 2,
       });
-      expect(doc.snapshotAt(3)).toEqual({
+      expect(replica.snapshotAt(3)).toEqual({
         visible: "Hello, Brian",
         hidden: "h Wworld",
         hiddenSeq: [0, 1, 1, 11, 7],
         version: 3,
       });
-      expect(doc.snapshotAt(4)).toEqual({
+      expect(replica.snapshotAt(4)).toEqual({
         visible: "Hello, Brian, Dr. Evil",
         hidden: "h Wworld",
         hiddenSeq: [0, 1, 1, 21, 7],
         version: 4,
       });
-      expect(doc.snapshotAt(4)).toEqual(doc.snapshot);
+      expect(replica.snapshotAt(4)).toEqual(replica.snapshot);
     });
   });
 
   describe("patchAt", () => {
     test("comprehensive", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([0, 11, "!", 11]);
-      doc.edit([0, 5, "_", 6, 12]);
-      doc.edit([0, 11, 12]);
-      expect(doc.patchAt(0)).toEqual(["hello world", 0]);
-      expect(doc.patchAt(1)).toEqual([0, 11, "!", 11]);
-      expect(doc.patchAt(2)).toEqual([0, 5, "_", 6, 12]);
-      expect(doc.patchAt(3)).toEqual([0, 11, 12]);
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([0, 11, "!", 11]);
+      replica.edit([0, 5, "_", 6, 12]);
+      replica.edit([0, 11, 12]);
+      expect(replica.patchAt(0)).toEqual(["hello world", 0]);
+      expect(replica.patchAt(1)).toEqual([0, 11, "!", 11]);
+      expect(replica.patchAt(2)).toEqual([0, 5, "_", 6, 12]);
+      expect(replica.patchAt(3)).toEqual([0, 11, 12]);
     });
   });
 
   describe("Replica.edit", () => {
     test("simple", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([0, 1, "era", 9, 11]);
-      expect(doc.snapshot).toEqual({
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([0, 1, "era", 9, 11]);
+      expect(replica.snapshot).toEqual({
         visible: "herald",
         hidden: "ello wor",
         hiddenSeq: [0, 4, 8, 2],
@@ -80,54 +80,54 @@ describe("Replica", () => {
     });
 
     test("sequential 1", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([0, 1, "era", 9, 11]);
-      doc.edit([0, 6, "ry", 6]);
-      expect(doc.snapshot.visible).toEqual("heraldry");
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([0, 1, "era", 9, 11]);
+      replica.edit([0, 6, "ry", 6]);
+      expect(replica.snapshot.visible).toEqual("heraldry");
     });
 
     test("sequential 2", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit(["H", 1, 6, "W", 7, 11]);
-      doc.edit([0, 5, ", Brian", 11]);
-      expect(doc.snapshot.visible).toEqual("Hello, Brian");
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit(["H", 1, 6, "W", 7, 11]);
+      replica.edit([0, 5, ", Brian", 11]);
+      expect(replica.snapshot.visible).toEqual("Hello, Brian");
     });
 
     test("concurrent 1", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([6, 11]);
-      doc.edit(["hello ", 0, 5]);
-      doc.edit(["goodbye ", 6, 11], { priority: 1, parent: 1 });
-      expect(doc.snapshot.visible).toEqual("goodbye hello world");
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([6, 11]);
+      replica.edit(["hello ", 0, 5]);
+      replica.edit(["goodbye ", 6, 11], { priority: 1, parent: 1 });
+      expect(replica.snapshot.visible).toEqual("goodbye hello world");
     });
 
     test("concurrent 2", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([0, 1, "era", 9, 11]);
-      doc.edit(["Great H", 2, 5, 11], { priority: 1, parent: 1 });
-      expect(doc.snapshot.visible).toEqual("Great Hera");
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([0, 1, "era", 9, 11]);
+      replica.edit(["Great H", 2, 5, 11], { priority: 1, parent: 1 });
+      expect(replica.snapshot.visible).toEqual("Great Hera");
     });
 
     test("concurrent 3", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit(["H", 1, 6, "W", 7, 11]);
-      doc.edit([0, 5, ", Brian", 11]);
-      doc.edit([0, 5, ", Dr. Evil", 11], { priority: 1, parent: 2 });
-      expect(doc.snapshot.visible).toEqual("Hello, Brian, Dr. Evil");
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit(["H", 1, 6, "W", 7, 11]);
+      replica.edit([0, 5, ", Brian", 11]);
+      replica.edit([0, 5, ", Dr. Evil", 11], { priority: 1, parent: 2 });
+      expect(replica.snapshot.visible).toEqual("Hello, Brian, Dr. Evil");
     });
 
     test("concurrent 4", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([6, 11]);
-      doc.edit(["hey ", 0, 5]);
-      doc.edit(["goodbye ", 6, 11], { priority: 1, parent: 1 });
-      expect(doc.snapshot).toEqual({
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([6, 11]);
+      replica.edit(["hey ", 0, 5]);
+      replica.edit(["goodbye ", 6, 11], { priority: 1, parent: 1 });
+      expect(replica.snapshot).toEqual({
         visible: "goodbye hey world",
         hidden: "hello ",
         hiddenSeq: [0, 8, 6, 9],
@@ -136,12 +136,12 @@ describe("Replica", () => {
     });
 
     test("concurrent 5", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([6, 11]);
-      doc.edit(["hey ", 0, 5], { priority: 1 });
-      doc.edit(["goodbye ", 0, 5], { parent: 2 });
-      expect(doc.snapshot).toEqual({
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([6, 11]);
+      replica.edit(["hey ", 0, 5], { priority: 1 });
+      replica.edit(["goodbye ", 0, 5], { parent: 2 });
+      expect(replica.snapshot).toEqual({
         visible: "goodbye hey world",
         hidden: "hello ",
         hiddenSeq: [1, 6, 17],
@@ -150,12 +150,12 @@ describe("Replica", () => {
     });
 
     test("concurrent 6", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit([6, 11]);
-      doc.edit(["hey ", 0, 5]);
-      doc.edit(["goodbye ", 6, 11], { priority: 1, parent: 1 });
-      expect(doc.snapshot).toEqual({
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit([6, 11]);
+      replica.edit(["hey ", 0, 5]);
+      replica.edit(["goodbye ", 6, 11], { priority: 1, parent: 1 });
+      expect(replica.snapshot).toEqual({
         visible: "goodbye hey world",
         hidden: "hello ",
         hiddenSeq: [0, 8, 6, 9],
@@ -164,139 +164,139 @@ describe("Replica", () => {
     });
 
     test("concurrent 7", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
       //"hello world"
       // ===========++++
-      doc.edit(["why ", 0, 5, " there", 5, 11, "s", 11]);
+      replica.edit(["why ", 0, 5, " there", 5, 11, "s", 11]);
       // ++++=====++++++======+
       //"why hello there worlds"
-      doc.edit([0, 11, "star", 11], { priority: 1, parent: 1 });
+      replica.edit([0, 11, "star", 11], { priority: 1, parent: 1 });
       // ======================++++
       //"why hello there worldsstar"
-      expect(doc.snapshot.visible).toEqual("why hello there worldsstar");
+      expect(replica.snapshot.visible).toEqual("why hello there worldsstar");
     });
   });
 
   describe("Replica.revert", () => {
     test("simple", () => {
-      const doc = new Replica("client1");
-      doc.edit(["hello world", 0]);
-      doc.edit(["goodbye", 5, 11]);
-      doc.edit([0, 13, "s", 13]);
-      doc.revert(1);
-      expect(doc.snapshot.visible).toEqual("hello worlds");
+      const replica = new Replica("client1");
+      replica.edit(["hello world", 0]);
+      replica.edit(["goodbye", 5, 11]);
+      replica.edit([0, 13, "s", 13]);
+      replica.revert(1);
+      expect(replica.snapshot.visible).toEqual("hello worlds");
     });
   });
 
   describe("Replica.ingest", () => {
     test("simple 1", () => {
-      const doc1 = new Replica("client1");
-      doc1.edit(["hello world", 0]);
-      const [revision1] = doc1.pending;
-      doc1.ingest({ ...revision1, global: 0 });
-      const doc2 = doc1.clone("client2");
-      doc1.edit(["goodbye", 5, 11]);
-      doc1.edit([0, 13, "s", 13]);
-      doc2.edit([0, 5, "_", 6, 11]);
-      const [revision2] = doc2.pending;
-      doc1.ingest({ ...revision2, global: 1 });
-      expect(doc1.snapshot).toEqual({
+      const replica1 = new Replica("client1");
+      replica1.edit(["hello world", 0]);
+      const [revision1] = replica1.pending;
+      replica1.ingest({ ...revision1, global: 0 });
+      const replica2 = replica1.clone("client2");
+      replica1.edit(["goodbye", 5, 11]);
+      replica1.edit([0, 13, "s", 13]);
+      replica2.edit([0, 5, "_", 6, 11]);
+      const [revision2] = replica2.pending;
+      replica1.ingest({ ...revision2, global: 1 });
+      expect(replica1.snapshot).toEqual({
         visible: "goodbye_worlds",
         hidden: "hello ",
         hiddenSeq: [0, 7, 5, 1, 1, 6],
         version: 4,
       });
-      expect(doc1.hiddenSeqAt(1)).toEqual([0, 11]);
+      expect(replica1.hiddenSeqAt(1)).toEqual([0, 11]);
     });
 
     test("simple 2", () => {
-      const doc1 = new Replica("client1");
-      doc1.edit(["hello world", 0]);
-      const [revision1] = doc1.pending;
-      doc1.ingest({ ...revision1, global: 0 });
-      const doc2 = doc1.clone("client2");
-      doc1.edit(["goodbye", 5, 11]);
-      const [revision2] = doc1.pending;
-      doc1.ingest({ ...revision2, global: 1 });
-      doc2.ingest({ ...revision2, global: 1 });
-      doc2.edit([0, 7, "hello", 7, 13]);
-      const [revision3] = doc2.pending;
-      doc1.ingest({ ...revision3, global: 2 });
-      expect(doc1.snapshot).toEqual({
+      const replica1 = new Replica("client1");
+      replica1.edit(["hello world", 0]);
+      const [revision1] = replica1.pending;
+      replica1.ingest({ ...revision1, global: 0 });
+      const replica2 = replica1.clone("client2");
+      replica1.edit(["goodbye", 5, 11]);
+      const [revision2] = replica1.pending;
+      replica1.ingest({ ...revision2, global: 1 });
+      replica2.ingest({ ...revision2, global: 1 });
+      replica2.edit([0, 7, "hello", 7, 13]);
+      const [revision3] = replica2.pending;
+      replica1.ingest({ ...revision3, global: 2 });
+      expect(replica1.snapshot).toEqual({
         visible: "goodbyehello world",
         hidden: "hello",
         hiddenSeq: [0, 7, 5, 11],
         version: 3,
       });
-      expect(doc1.hiddenSeqAt(1)).toEqual([0, 11]);
+      expect(replica1.hiddenSeqAt(1)).toEqual([0, 11]);
     });
 
     test("idempotent", () => {
-      const doc1 = new Replica("client1");
-      doc1.edit(["hello world", 0]);
-      const [revision1] = doc1.pending;
-      doc1.ingest({ ...revision1, global: 0 });
-      const doc2 = doc1.clone("client2");
-      doc1.edit(["goodbye", 5, 11]);
-      const [revision2] = doc1.pending;
-      doc2.ingest({ ...revision2, global: 1 });
-      doc2.ingest({ ...revision2, global: 1 });
-      doc2.ingest({ ...revision2, global: 1 });
-      expect(doc2.snapshot).toEqual({
+      const replica1 = new Replica("client1");
+      replica1.edit(["hello world", 0]);
+      const [revision1] = replica1.pending;
+      replica1.ingest({ ...revision1, global: 0 });
+      const replica2 = replica1.clone("client2");
+      replica1.edit(["goodbye", 5, 11]);
+      const [revision2] = replica1.pending;
+      replica2.ingest({ ...revision2, global: 1 });
+      replica2.ingest({ ...revision2, global: 1 });
+      replica2.ingest({ ...revision2, global: 1 });
+      expect(replica2.snapshot).toEqual({
         visible: "goodbye world",
         hidden: "hello",
         hiddenSeq: [0, 7, 5, 6],
         version: 2,
       });
-      expect(doc2.snapshot).toEqual(doc1.snapshot);
-      expect(doc1.hiddenSeqAt(1)).toEqual([0, 11]);
+      expect(replica2.snapshot).toEqual(replica1.snapshot);
+      expect(replica1.hiddenSeqAt(1)).toEqual([0, 11]);
     });
 
     test("concurrent 1", () => {
-      const doc1 = new Replica("client1");
-      doc1.edit(["hello world", 0]);
+      const replica1 = new Replica("client1");
+      replica1.edit(["hello world", 0]);
       let global = 0;
-      for (const rev of doc1.pending) {
-        doc1.ingest({ ...rev, global });
+      for (const rev of replica1.pending) {
+        replica1.ingest({ ...rev, global });
         global += 1;
       }
-      const doc2 = doc1.clone("client2");
-      doc1.edit(["H", 1, 6, "W", 7, 11]);
-      doc1.edit([0, 5, 6, 11]);
-      doc2.edit([0, 5, "_", 6, 11, "!", 11]);
-      doc2.edit([0, 11, 12]);
-      const revisions = doc1.pending.concat(doc2.pending);
+      const replica2 = replica1.clone("client2");
+      replica1.edit(["H", 1, 6, "W", 7, 11]);
+      replica1.edit([0, 5, 6, 11]);
+      replica2.edit([0, 5, "_", 6, 11, "!", 11]);
+      replica2.edit([0, 11, 12]);
+      const revisions = replica1.pending.concat(replica2.pending);
       for (const rev of revisions) {
-        doc1.ingest({ ...rev, global });
-        doc2.ingest({ ...rev, global });
+        replica1.ingest({ ...rev, global });
+        replica2.ingest({ ...rev, global });
         global += 1;
       }
-      expect(doc1.snapshot).toEqual(doc2.snapshot);
-      expect(doc1.hiddenSeqAt(1)).toEqual([0, 11]);
+      expect(replica1.snapshot).toEqual(replica2.snapshot);
+      expect(replica1.hiddenSeqAt(1)).toEqual([0, 11]);
     });
 
     test("concurrent 2", () => {
-      const doc1 = new Replica("client1");
-      doc1.edit(["hello world", 0]);
+      const replica1 = new Replica("client1");
+      replica1.edit(["hello world", 0]);
       let global = 0;
-      for (const rev of doc1.pending) {
-        doc1.ingest({ ...rev, global });
+      for (const rev of replica1.pending) {
+        replica1.ingest({ ...rev, global });
         global += 1;
       }
-      const doc2 = doc1.clone("client2");
-      doc1.edit(["H", 1, 6, "W", 7, 11]);
-      doc1.edit([0, 5, 6, 11]);
-      doc2.edit([0, 5, "_", 6, 11, "!", 11]);
-      doc2.edit([0, 11, 12]);
-      const revisions = doc2.pending.concat(doc1.pending);
+      const replica2 = replica1.clone("client2");
+      replica1.edit(["H", 1, 6, "W", 7, 11]);
+      replica1.edit([0, 5, 6, 11]);
+      replica2.edit([0, 5, "_", 6, 11, "!", 11]);
+      replica2.edit([0, 11, 12]);
+      const revisions = replica2.pending.concat(replica1.pending);
       for (const rev of revisions) {
-        doc1.ingest({ ...rev, global });
-        doc2.ingest({ ...rev, global });
+        replica1.ingest({ ...rev, global });
+        replica2.ingest({ ...rev, global });
         global += 1;
       }
-      expect(doc1.snapshot).toEqual(doc2.snapshot);
-      expect(doc1.hiddenSeqAt(1)).toEqual([0, 11]);
+      expect(replica1.snapshot).toEqual(replica2.snapshot);
+      expect(replica1.hiddenSeqAt(1)).toEqual([0, 11]);
     });
   });
 });
