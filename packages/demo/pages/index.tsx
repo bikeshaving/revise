31470bc7d099298ff3a6549935303408f95eb914
Brian Kim <briankimpossible@gmail.com>
2019-03-29 19:04:18 -0400

    🤪 holy shit a working demo 🤪

--- packages/demo/pages/index.tsx
+++ packages/demo/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as uuid from "uuid/v4";
 import { WebSocketConnection } from "../websocket/connection";
 import { Client } from "@collabjs/collab/lib/client";
+import { operations } from "@collabjs/collab/lib/patch";
 import { CollabText } from "@collabjs/collab/lib/text";
 
 let CodeMirror: any;
@@ -27,21 +28,42 @@ function Editor() {
     ): void {
       if (change.origin === "setValue") {
         return;
+      } else if (change.origin === "collab") {
+        return;
       }
       const start = cm.indexFromPos(change.from);
       const end = start + cm.getRange(change.from, change.to).length;
       const inserted = change.text.join("\n");
-      text!.replace(start, end, inserted);
+      text.replace(start, end, inserted);
     }
-    CollabText.initialize("doc1", client).then((collabText) => {
-      text = collabText;
-      cm.setValue(collabText.text);
+    CollabText.initialize("doc1", client).then(async (text1) => {
+      text = text1;
+      cm.setValue(text.text);
       cm.on("beforeChange", handleBeforeChange);
       cm.setOption("readOnly", false);
+      for await (const revision of text.subscribe()) {
+        cm.operation(() => {
+          if (revision.client !== client.id) {
+            for (const op of operations(revision.patch)) {
+              switch (op.type) {
+                case "delete": {
+                  const start = cm.posFromIndex(op.start);
+                  const end = cm.posFromIndex(op.end);
+                  cm.replaceRange("", start, end, "collab");
+                  break;
+                }
+                case "insert": {
+                  const start = cm.posFromIndex(op.start);
+                  cm.replaceRange(op.inserted, start, null, "collab");
+                  break;
+                }
+              }
+            }
+          }
+        });
+      }
     });
-    return () => {
-      cm.off("beforeChange", handleBeforeChange);
-    };
+    return () => cm.off("beforeChange", handleBeforeChange);
   }, []);
   return <div ref={editorNode} />;
 }
