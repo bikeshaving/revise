b422d7a1162ec2341977034f6ec99e06eeb6b6a8
Brian Kim <briankimpossible@gmail.com>
2021-09-15 02:56:29 -0400

    revive the old history implementation

---
+++ src/history.ts
@@ -0,0 +1,87 @@
+import {Edit} from "./edit";
+
+// TODO: Figure out if these make sense as Edit methods.
+function isNoop(edit: Edit): boolean {
+	// TODO: Think about if the second part of this condition is actually
+	// necessary
+	return edit.parts.length === 1 && typeof edit.parts[0] === "number";
+}
+
+function isComplex(edit: Edit): boolean {
+	let count = 0;
+	for (const op of edit.operations) {
+		if (op.type !== "retain") {
+			count++;
+			if (count > 1) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+export class EditHistory {
+	current: Edit | undefined;
+	undoStack: Array<Edit>;
+	redoStack: Array<Edit>;
+
+	constructor() {
+		this.current = undefined;
+		this.undoStack = [];
+		this.redoStack = [];
+	}
+
+	checkpoint(): void {
+		if (this.current) {
+			this.undoStack.push(this.current);
+			this.current = undefined;
+		}
+	}
+
+	push(edit: Edit): void {
+		if (isNoop(edit)) {
+			return;
+		} else if (this.redoStack.length) {
+			this.redoStack.length = 0;
+		}
+
+		if (this.current) {
+			const oldEdit = this.current;
+			if (!isComplex(oldEdit) && !isComplex(edit)) {
+				this.current = oldEdit.compose(edit);
+				return;
+			} else {
+				this.checkpoint();
+			}
+		}
+
+		this.current = edit;
+	}
+
+	canUndo(): boolean {
+		return !!(this.current || this.undoStack.length);
+	}
+
+	undo(): Edit | undefined {
+		this.checkpoint();
+		const edit = this.undoStack.pop();
+		if (edit) {
+			this.redoStack.push(edit);
+			return edit.invert();
+		}
+	}
+
+	canRedo(): boolean {
+		return !!this.redoStack.length;
+	}
+
+	redo(): Edit | undefined {
+		this.checkpoint();
+		const edit = this.redoStack.pop();
+		if (edit) {
+			this.undoStack.push(edit);
+			return edit;
+		}
+	}
+}
