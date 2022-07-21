da44ce57f9e637cfb1b9757f1d0607bb4e8ec87f
Brian Kim <brainkim@fuckyou.com>
2022-07-21 00:49:02 -0700

    document edit a little bit and hide some methods

--- src/edit.ts
+++ src/edit.ts
@@ -1,4 +1,10 @@
-import {Subseq} from "./subseq";
+import {Subseq} from "./subseq.js";
+
+export interface InsertOperation {
+	type: "insert";
+	start: number;
+	value: string;
+}
 
 export interface RetainOperation {
 	type: "retain";
@@ -13,25 +19,45 @@ export interface DeleteOperation {
 	value: string | undefined;
 }
 
-export interface InsertOperation {
-	type: "insert";
-	start: number;
-	value: string;
-}
-
 export type Operation = RetainOperation | DeleteOperation | InsertOperation;
 
-/**
- * A data structure which represents edits to strings.
- */
+/** A data structure which represents edits to strings. */
 export class Edit {
-	/** An array of strings and numbers representing operations. */
+	/**
+	 * An array of strings and integers representing operations.
+	 *
+	 * Strings represent insertions, and pairs of integers represent the start
+	 * and end indices of retained segments. Deletions are implied by gaps
+	 * between the retained segments.
+	 *
+	 * Examples:
+	 *
+	 *   [0, 3, 7, 10]
+	 *   retain 0-3, delete 3-7, retain 7-10
+	 *
+	 *   ["hello", 0, 10]
+	 *   insert "hello", retain 0-10
+	 *
+	 *   [0, 5, " ", 6, 10]
+	 *   retain 0-5, insert " ", delete 5-6, retain 6-10
+	 *
+	 * If the edit includes a delete operation at its end, this is signified by
+	 * an extra number signifying the length. Therefore, regardless of whether or
+	 * not the last character of the string is retained, the last number will
+	 * always represent the length of the original string.
+	 *
+	 *   // retain 0-10, delete 10-11
+	 *   [0, 10, 11]
+	 *
+	 * An edit that is only retains will contain a single number representing the
+	 * length of the string.
+	 */
 	parts: Array<string | number>;
 
-	/**
-	 * A string which represents a concatenation of all deletions.
+	/** A string which represents a concatenation of all deletions.
 	 *
-	 * This property must be provided if you want to invert the edit.
+	 * This property is optional, but necessary if you want to call the invert()
+	 * method on an edit.
 	 */
 	deleted: string | undefined;
 
@@ -40,105 +66,7 @@ export class Edit {
 		this.deleted = deleted;
 	}
 
-	static synthesize(
-		insertSeq: Subseq,
-		inserted: string,
-		deleteSeq: Subseq,
-		deleted?: string | undefined,
-	): Edit {
-		if (insertSeq.includedSize !== inserted.length) {
-			throw new Error("insertSeq and inserted string do not match in length");
-		} else if (
-			deleted !== undefined &&
-			deleteSeq.includedSize !== deleted.length
-		) {
-			throw new Error("deleteSeq and deleted string do not match in length");
-		} else if (deleteSeq.size !== insertSeq.excludedSize) {
-			throw new Error("deleteSeq and insertSeq do not match in length");
-		}
-
-		const parts: Array<string | number> = [];
-		let insertIndex = 0;
-		let retainIndex = 0;
-		let needsLength = true;
-		for (const [length, deleting, inserting] of deleteSeq
-			.expand(insertSeq)
-			.align(insertSeq)) {
-			if (inserting) {
-				parts.push(inserted.slice(insertIndex, insertIndex + length));
-				insertIndex += length;
-			} else {
-				if (!deleting) {
-					parts.push(retainIndex, retainIndex + length);
-				}
-
-				retainIndex += length;
-				needsLength = deleting;
-			}
-		}
-
-		if (needsLength) {
-			parts.push(retainIndex);
-		}
-
-		return new Edit(parts, deleted);
-	}
-
-	static build(
-		text: string,
-		inserted: string,
-		from: number,
-		to: number = from,
-	): Edit {
-		const insertSizes: Array<number> = [];
-		Subseq.pushSegment(insertSizes, from, false);
-		Subseq.pushSegment(insertSizes, inserted.length, true);
-		Subseq.pushSegment(insertSizes, to - from, false);
-		Subseq.pushSegment(insertSizes, text.length - to, false);
-		const deleteSizes: Array<number> = [];
-		Subseq.pushSegment(deleteSizes, from, false);
-		Subseq.pushSegment(deleteSizes, to - from, true);
-		Subseq.pushSegment(deleteSizes, text.length - to, false);
-		const deleted = text.slice(from, to);
-		return Edit.synthesize(
-			new Subseq(insertSizes),
-			inserted,
-			new Subseq(deleteSizes),
-			deleted,
-		);
-	}
-
-	/**
-	 * Given two strings, this method finds an edit which can be applied to the
-	 * first string to result in the second.
-	 * An optional hint can be provided to disambiguate edits when they are made
-	 * to overlapping characters.
-	 */
-	static diff(text1: string, text2: string, hint?: number): Edit {
-		let prefix = commonPrefixLength(text1, text2);
-		let suffix = commonSuffixLength(text1, text2);
-		// prefix and suffix overlap when edits are runs of the same character.
-		if (prefix + suffix > Math.min(text1.length, text2.length)) {
-			if (hint !== undefined && hint > -1) {
-				prefix = Math.min(prefix, hint);
-			}
-
-			// TODO: We can probably avoid the commonSuffixLength() call here in
-			// favor of arithmetic but I’m too dumb to figure it out.
-			suffix = commonSuffixLength(text1.slice(prefix), text2.slice(prefix));
-		}
-
-		return Edit.build(
-			text1,
-			text2.slice(prefix, text2.length - suffix),
-			prefix,
-			text1.length - suffix,
-		);
-	}
-
-	/**
-	 * A string which represents a concatenation of all insertions.
-	 */
+	/** A string which represents a concatenation of all insertions. */
 	get inserted(): string {
 		let text = "";
 		for (let i = 0; i < this.parts.length; i++) {
@@ -150,6 +78,20 @@ export class Edit {
 		return text;
 	}
 
+	/**
+	 * Returns an array of operations, which is more readable than the parts
+	 * array.
+	 *
+	 *   new Edit([0, 1, " ", 2], "x").operations();
+	 *   [
+	 *     {type: "retain", start: 0, end: 1},
+	 *     {type: "insert", start: 1, value: " "},
+	 *     {type: "delete", start: 1, end: 2, value: "x"},
+	 *   ]
+	 *
+	 * When insertions and deletions happen at the same index, insertions will
+	 * always appear before deletions in the operations array.
+	 */
 	operations(): Array<Operation> {
 		const operations: Array<Operation> = [];
 		let retaining = false;
@@ -208,111 +150,10 @@ export class Edit {
 		return text1;
 	}
 
-	factor(): [Subseq, string, Subseq, string | undefined] {
-		const insertSizes: Array<number> = [];
-		const deleteSizes: Array<number> = [];
-		let inserted = "";
-		const operations = this.operations();
-		for (let i = 0; i < operations.length; i++) {
-			const op = operations[i];
-			switch (op.type) {
-				case "retain": {
-					const length = op.end - op.start;
-					Subseq.pushSegment(insertSizes, length, false);
-					Subseq.pushSegment(deleteSizes, length, false);
-					break;
-				}
-				case "delete": {
-					const length = op.end - op.start;
-					Subseq.pushSegment(insertSizes, length, false);
-					Subseq.pushSegment(deleteSizes, length, true);
-					break;
-				}
-				case "insert":
-					Subseq.pushSegment(insertSizes, op.value.length, true);
-					inserted += op.value;
-					break;
-				default:
-					throw new TypeError("Invalid operation type");
-			}
-		}
-
-		const insertSeq = new Subseq(insertSizes);
-		const deleteSeq = new Subseq(deleteSizes);
-		return [insertSeq, inserted, deleteSeq, this.deleted];
-	}
-
-	normalize(): Edit {
-		if (typeof this.deleted === "undefined") {
-			throw new Error("Missing deleted property");
-		}
-
-		const insertSizes: Array<number> = [];
-		const deleteSizes: Array<number> = [];
-		let inserted = "";
-		let deleted = "";
-		let prevInserted: string | undefined;
-		const operations = this.operations();
-		for (let i = 0; i < operations.length; i++) {
-			const op = operations[i];
-			switch (op.type) {
-				case "insert": {
-					prevInserted = op.value;
-					break;
-				}
-
-				case "retain": {
-					if (prevInserted !== undefined) {
-						Subseq.pushSegment(insertSizes, prevInserted.length, true);
-						inserted += prevInserted;
-						prevInserted = undefined;
-					}
-
-					Subseq.pushSegment(insertSizes, op.end - op.start, false);
-					Subseq.pushSegment(deleteSizes, op.end - op.start, false);
-					break;
-				}
-
-				case "delete": {
-					const length = op.end - op.start;
-					let prefix = 0;
-					if (prevInserted !== undefined) {
-						prefix = commonPrefixLength(prevInserted, op.value!);
-						Subseq.pushSegment(insertSizes, prefix, false);
-						Subseq.pushSegment(insertSizes, prevInserted.length - prefix, true);
-						inserted += prevInserted.slice(prefix);
-						prevInserted = undefined;
-					}
-
-					deleted += op.value!.slice(prefix);
-					Subseq.pushSegment(deleteSizes, prefix, false);
-					Subseq.pushSegment(deleteSizes, length - prefix, true);
-					Subseq.pushSegment(insertSizes, length - prefix, false);
-					break;
-				}
-
-				default: {
-					throw new TypeError("Invalid operation type");
-				}
-			}
-		}
-
-		if (prevInserted !== undefined) {
-			Subseq.pushSegment(insertSizes, prevInserted.length, true);
-			inserted += prevInserted;
-		}
-
-		const insertSeq = new Subseq(insertSizes);
-		const deleteSeq = new Subseq(deleteSizes);
-		return Edit.synthesize(insertSeq, inserted, deleteSeq, deleted);
-	}
-
-	/**
-	 * Composes two consecutive edits.
-	 */
+	/** Composes two consecutive edits. */
 	compose(that: Edit): Edit {
-		let [insertSeq1, inserted1, deleteSeq1, deleted1] = this.factor();
-		let [insertSeq2, inserted2, deleteSeq2, deleted2] = that.factor();
+		let [insertSeq1, inserted1, deleteSeq1, deleted1] = factor(this);
+		let [insertSeq2, inserted2, deleteSeq2, deleted2] = factor(that);
 		// Expand all subseqs so that they share the same coordinate space.
 		deleteSeq1 = deleteSeq1.expand(insertSeq1);
 		deleteSeq2 = deleteSeq2.expand(deleteSeq1);
@@ -339,7 +180,7 @@ export class Edit {
 			deleted1 != null && deleted2 != null
 				? consolidate(deleteSeq1, deleted1, deleteSeq2, deleted2)
 				: undefined;
-		return Edit.synthesize(insertSeq, inserted, deleteSeq, deleted).normalize();
+		return normalize(Edit.synthesize(insertSeq, inserted, deleteSeq, deleted));
 	}
 
 	invert(): Edit {
@@ -347,11 +188,208 @@ export class Edit {
 			throw new Error("Missing deleted property");
 		}
 
-		let [insertSeq, inserted, deleteSeq, deleted] = this.factor();
+		let [insertSeq, inserted, deleteSeq, deleted] = factor(this);
 		deleteSeq = deleteSeq.expand(insertSeq);
 		insertSeq = insertSeq.shrink(deleteSeq);
 		return Edit.synthesize(deleteSeq, deleted!, insertSeq, inserted);
 	}
+
+	static synthesize(
+		insertSeq: Subseq,
+		inserted: string,
+		deleteSeq: Subseq,
+		deleted?: string | undefined,
+	): Edit {
+		if (insertSeq.includedSize !== inserted.length) {
+			throw new Error("insertSeq and inserted string do not match in length");
+		} else if (
+			deleted !== undefined &&
+			deleteSeq.includedSize !== deleted.length
+		) {
+			throw new Error("deleteSeq and deleted string do not match in length");
+		} else if (deleteSeq.size !== insertSeq.excludedSize) {
+			throw new Error("deleteSeq and insertSeq do not match in length");
+		}
+
+		const parts: Array<string | number> = [];
+		let insertIndex = 0;
+		let retainIndex = 0;
+		let needsLength = true;
+		for (const [length, deleting, inserting] of deleteSeq
+			.expand(insertSeq)
+			.align(insertSeq)) {
+			if (inserting) {
+				parts.push(inserted.slice(insertIndex, insertIndex + length));
+				insertIndex += length;
+			} else {
+				if (!deleting) {
+					parts.push(retainIndex, retainIndex + length);
+				}
+
+				retainIndex += length;
+				needsLength = deleting;
+			}
+		}
+
+		if (needsLength) {
+			parts.push(retainIndex);
+		}
+
+		return new Edit(parts, deleted);
+	}
+
+	// TODO: I wish we had a way to build edits. Take inspiration from Quill.js
+	// API maybe.
+	static build(
+		text: string,
+		inserted: string,
+		from: number,
+		to: number = from,
+	): Edit {
+		const insertSizes: Array<number> = [];
+		Subseq.pushSegment(insertSizes, from, false);
+		Subseq.pushSegment(insertSizes, inserted.length, true);
+		Subseq.pushSegment(insertSizes, to - from, false);
+		Subseq.pushSegment(insertSizes, text.length - to, false);
+		const deleteSizes: Array<number> = [];
+		Subseq.pushSegment(deleteSizes, from, false);
+		Subseq.pushSegment(deleteSizes, to - from, true);
+		Subseq.pushSegment(deleteSizes, text.length - to, false);
+		const deleted = text.slice(from, to);
+		return Edit.synthesize(
+			new Subseq(insertSizes),
+			inserted,
+			new Subseq(deleteSizes),
+			deleted,
+		);
+	}
+
+	/**
+	 * Given two strings, this method finds an edit which can be applied to the
+	 * first string to result in the second.
+	 *
+	 * @param hint - An optional hint can be provided to disambiguate edits which
+	 * cannot be inferred from the text alone, for example, inserting "a" into
+	 * the string "aaaa" could be an insertion at any point in the string which
+	 * leads to the same result. The hint is usually inferred from the user
+	 * interface.
+	 */
+	static diff(text1: string, text2: string, hint?: number): Edit {
+		let prefix = commonPrefixLength(text1, text2);
+		let suffix = commonSuffixLength(text1, text2);
+		// prefix and suffix overlap when edits are in runs of the same character.
+		if (prefix + suffix > Math.min(text1.length, text2.length)) {
+			if (hint != null && hint >= 0) {
+				prefix = Math.min(prefix, hint);
+			}
+
+			// TODO: We can probably avoid the commonSuffixLength() call here in
+			// favor of arithmetic but I’m too dumb to figure it out.
+			suffix = commonSuffixLength(text1.slice(prefix), text2.slice(prefix));
+		}
+
+		return Edit.build(
+			text1,
+			text2.slice(prefix, text2.length - suffix),
+			prefix,
+			text1.length - suffix,
+		);
+	}
+}
+
+function factor(edit: Edit): [Subseq, string, Subseq, string | undefined] {
+	const insertSizes: Array<number> = [];
+	const deleteSizes: Array<number> = [];
+	let inserted = "";
+	const operations = edit.operations();
+	for (let i = 0; i < operations.length; i++) {
+		const op = operations[i];
+		switch (op.type) {
+			case "retain": {
+				const length = op.end - op.start;
+				Subseq.pushSegment(insertSizes, length, false);
+				Subseq.pushSegment(deleteSizes, length, false);
+				break;
+			}
+			case "delete": {
+				const length = op.end - op.start;
+				Subseq.pushSegment(insertSizes, length, false);
+				Subseq.pushSegment(deleteSizes, length, true);
+				break;
+			}
+			case "insert":
+				Subseq.pushSegment(insertSizes, op.value.length, true);
+				inserted += op.value;
+				break;
+			default:
+				throw new TypeError("Invalid operation type");
+		}
+	}
+
+	const insertSeq = new Subseq(insertSizes);
+	const deleteSeq = new Subseq(deleteSizes);
+	return [insertSeq, inserted, deleteSeq, edit.deleted];
+}
+
+function normalize(edit: Edit): Edit {
+	if (typeof edit.deleted === "undefined") {
+		throw new Error("Missing deleted property");
+	}
+
+	const insertSizes: Array<number> = [];
+	const deleteSizes: Array<number> = [];
+	let inserted = "";
+	let deleted = "";
+	let prevInserted: string | undefined;
+	const operations = edit.operations();
+	for (let i = 0; i < operations.length; i++) {
+		const op = operations[i];
+		switch (op.type) {
+			case "insert": {
+				prevInserted = op.value;
+				break;
+			}
+
+			case "retain": {
+				if (prevInserted !== undefined) {
+					Subseq.pushSegment(insertSizes, prevInserted.length, true);
+					inserted += prevInserted;
+					prevInserted = undefined;
+				}
+
+				Subseq.pushSegment(insertSizes, op.end - op.start, false);
+				Subseq.pushSegment(deleteSizes, op.end - op.start, false);
+				break;
+			}
+
+			case "delete": {
+				const length = op.end - op.start;
+				let prefix = 0;
+				if (prevInserted !== undefined) {
+					prefix = commonPrefixLength(prevInserted, op.value!);
+					Subseq.pushSegment(insertSizes, prefix, false);
+					Subseq.pushSegment(insertSizes, prevInserted.length - prefix, true);
+					inserted += prevInserted.slice(prefix);
+					prevInserted = undefined;
+				}
+
+				deleted += op.value!.slice(prefix);
+				Subseq.pushSegment(deleteSizes, prefix, false);
+				Subseq.pushSegment(deleteSizes, length - prefix, true);
+				Subseq.pushSegment(insertSizes, length - prefix, false);
+				break;
+			}
+		}
+	}
+
+	if (prevInserted !== undefined) {
+		Subseq.pushSegment(insertSizes, prevInserted.length, true);
+		inserted += prevInserted;
+	}
+
+	const insertSeq = new Subseq(insertSizes);
+	const deleteSeq = new Subseq(deleteSizes);
+	return Edit.synthesize(insertSeq, inserted, deleteSeq, deleted);
 }
 
 /**
@@ -412,9 +450,7 @@ function erase(subseq1: Subseq, str: string, subseq2: Subseq): string {
 	return result;
 }
 
-/**
- * @returns The length of the common prefix between two strings.
- */
+/** @returns The length of the common prefix between two strings. */
 function commonPrefixLength(text1: string, text2: string) {
 	const length = Math.min(text1.length, text2.length);
 	for (let i = 0; i < length; i++) {
@@ -426,9 +462,7 @@ function commonPrefixLength(text1: string, text2: string) {
 	return length;
 }
 
-/**
- * @returns The length of the common suffix between two strings.
- */
+/** @returns The length of the common suffix between two strings. */
 function commonSuffixLength(text1: string, text2: string) {
 	const length1 = text1.length;
 	const length2 = text2.length;
